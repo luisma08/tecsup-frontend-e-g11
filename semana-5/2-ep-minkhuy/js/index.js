51e506801c1ba1d73717645265a54c1b63865072
Jorge Mayta <luismayta08@gmail.com>
2022-12-07 22:00:50 -0500

    add class with js

--- semana-5/2-ep-minkhuy/js/index.js
+++ semana-5/2-ep-minkhuy/js/index.js
@@ -1,7 +1,17 @@
 'use strict';
 
-const navMenuIcon=document.getElementById('nav-menu-icon');
+const iconMenu=document.getElementById('icon-menu');
 const listHeader=document.getElementById('list-header');
-navMenuIcon.addEventListener('click', () => {
-    listHeader.style.opacity = '1';
+const iconClose=document.getElementById('icon-close');
+
+iconMenu.addEventListener('click', () => {
+    //listHeader.style.pointerEvents='auto';
+    //listHeader.style.opacity = '1';
+    listHeader.classList.add('list--header-show');
+});
+
+iconClose.addEventListener('click', () => {
+    //listHeader.style.pointerEvents='none';
+    //listHeader.style.opacity = '0';
+    listHeader.classList.remove('list--header-show');
 });
