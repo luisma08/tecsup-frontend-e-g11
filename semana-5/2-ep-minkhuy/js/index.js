02fa51fe92732e17e00ccd80897c06c0f57ef2cb
Jorge Mayta <luismayta08@gmail.com>
2022-12-07 20:57:00 -0500

    add partial icons

--- semana-5/2-ep-minkhuy/js/index.js
+++ semana-5/2-ep-minkhuy/js/index.js
@@ -1,1 +1,7 @@
-alert("hola mundo");
+'use strict';
+
+const navMenuIcon=document.getElementById('nav-menu-icon');
+const listHeader=document.getElementById('list-header');
+navMenuIcon.addEventListener('click', () => {
+    listHeader.style.opacity = '1';
+});
