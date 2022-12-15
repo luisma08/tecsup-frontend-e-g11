87e45178644c5e47609444871837cbe765b67e35
Jorge Mayta <luismayta08@gmail.com>
2022-12-15 15:15:59 -0500

    update js

--- semana-7/1-crud-pokemons/js/index.js
+++ semana-7/1-crud-pokemons/js/index.js
@@ -1,7 +1,49 @@
 'use strict'
 
 const documentReady = () => {
-    alert('Jorge Mayta');
+    const pokemons = []
+    const formPokemon = document.getElementById('formPokemon');
+    /* `Plantillas literales` */
+
+    const createPokemon = (e) => {
+        e.preventDefault();
+        const documentFormPokemon = document.forms['formPokemon'];
+        const name = documentFormPokemon.name.value;
+        const type = documentFormPokemon.type.value;
+        const hp = documentFormPokemon.hp.value;
+        const attack = documentFormPokemon.attack.value;
+        const special = documentFormPokemon.special.value;
+        const imgUrl = documentFormPokemon.imgUrl.value;
+        pokemons.push({name, type, hp, attack, special, imgUrl});
+        readPokemons();
+    };
+
+    const readPokemons = () => {
+        //const {name, type, hp, attack, special, imgUrl} = pokemon;
+        const tBodyPokemons = document.getElementById('tBodyPokemons');
+        tBodyPokemons.innerHTML = '';
+        pokemons.forEach((element, index) => {
+            index+=1;
+            const {name, type, hp, attack, special, imgUrl} = element;
+            tBodyPokemons.innerHTML += `
+            <tr>
+                <td>${index}</td>
+                <td>${name}</td>
+                <td>${type}</td>
+                <td>${hp}</td>
+                <td>${attack}</td>
+                <td>${special}</td>
+                <td>
+                <img
+                    src="${imgUrl}"
+                    alt="${name}" class="img-fluid" style="max-width: 128px;" />
+                </td>
+            </tr>
+            `;
+        });
+    };
+
+    formPokemon.addEventListener('submit', createPokemon);
 };
 
 document.addEventListener('DOMContentLoaded', documentReady);
