4fa35c5edc049dacdab0a13fcff8cda0634a95fa
Jorge Mayta <luismayta08@gmail.com>
2022-12-14 19:16:52 -0500

    update js

--- semana-6/1-kierrez-zer-my-nobia/js/index.js
+++ semana-6/1-kierrez-zer-my-nobia/js/index.js
@@ -1,15 +1,25 @@
 'use strict';
 
 const documentReady = () => {
+    const heroTitle = document.getElementById('heroTitle')
     const yesButton = document.getElementById('yesButton');
     const noButton = document.getElementById('noButton');
 
     const brokenHeart = ()=>{
         noButton.style.top = Math.random() * innerHeight + 'px';
         noButton.style.left = Math.random() * innerWidth + 'px';
+        yesButton.classList.remove('emoji');
     }
 
+    const loved = ()=>{
+        yesButton.classList.add('emoji');
+    }
+
+    const partner = prompt('😍 Dime tu nombre 😍');
+    heroTitle.innerHTML += ` ${partner || 'Anonimo'} ? ❤ `;
+
     noButton.addEventListener('mouseover', brokenHeart);
+    yesButton.addEventListener('mouseover', loved)
 };
 
 document.addEventListener('DOMContentLoaded', documentReady);
