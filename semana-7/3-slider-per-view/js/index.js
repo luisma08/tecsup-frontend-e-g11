a9539071c6f791d64827a93123d374db7b9d6dd1
Jorge Mayta <luismayta08@gmail.com>
2022-12-17 17:44:51 -0500

    add navigation on js

--- semana-7/3-slider-per-view/js/index.js
+++ semana-7/3-slider-per-view/js/index.js
@@ -1,10 +1,44 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js';
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
+    slidesPerView: 1,
+    spaceBetween: 10,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+    autoplay:{
+        delay:500,
+    },
+
+    loop: true,
+
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 60,
+        },
+
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 80,
+        }
+      }
   });
