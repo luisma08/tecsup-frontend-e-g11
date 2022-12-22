6385f6a32b4b4574dee6742c0db00472806b0259
Jorge Mayta <luismayta08@gmail.com>
2022-12-21 21:54:39 -0500

    add async function and call json

--- semana-8/3-chanchirata/js/index.js
+++ semana-8/3-chanchirata/js/index.js
@@ -3,6 +3,11 @@
 import foods from "./modules/foods.js";
 
 const documetReady = () => {
-    foods();
+    const fetchApi = async () => {
+        const response = await fetch('http://127.0.0.1:5501/semana-8/3-chanchirata/json/foods.json');
+        const data = await response.json();
+        foods(data);
+    };
+    fetchApi();
 };
 document.addEventListener('DOMContentLoaded', documetReady);
--- semana-8/3-chanchirata/js/modules/foods.js
+++ semana-8/3-chanchirata/js/modules/foods.js
@@ -1,30 +1,8 @@
-const comidas = [
-    {
-      "id": 1,
-      "name": "Ají de Gallina",
-      "description": "El ají de gallina es un plato típico de la gastronomía peruana, concretamente de la costa, ​ que consiste en un ají o crema espesa con pechuga de gallina deshilachada. Esta crema es servida con papas cocidas, y en ocasiones con arroz blanco. Es común reemplazar la gallina por pollo.",
-      "urlImage": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Ají_de_gallina.jpg/800px-Ají_de_gallina.jpg",
-      "urlRecipe": "https://www.recetasnestle.com.pe/recetas/aji-de-gallina-2"
-    },
-    {
-      "id": 2,
-      "name": "Lomo Saltado",
-      "description": "El ají de gallina es un plato típico de la gastronomía peruana, concretamente de la costa, ​ que consiste en un ají o crema espesa con pechuga de gallina deshilachada. Esta crema es servida con papas cocidas, y en ocasiones con arroz blanco. Es común reemplazar la gallina por pollo.",
-      "urlImage": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Ají_de_gallina.jpg/800px-Ají_de_gallina.jpg",
-      "urlRecipe": "https://www.recetasnestle.com.pe/recetas/aji-de-gallina-2"
-    },
-    {
-      "id": 3,
-      "name": "Chaufa",
-      "description": "El ají de gallina es un plato típico de la gastronomía peruana, concretamente de la costa, ​ que consiste en un ají o crema espesa con pechuga de gallina deshilachada. Esta crema es servida con papas cocidas, y en ocasiones con arroz blanco. Es común reemplazar la gallina por pollo.",
-      "urlImage": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Ají_de_gallina.jpg/800px-Ají_de_gallina.jpg",
-      "urlRecipe": "https://www.recetasnestle.com.pe/recetas/aji-de-gallina-2"
-    }
-  ];
-  
-  const foods = () => {
+
+  const foods = ({ foods }) => {
     const foodsContainer = document.getElementById('foodsContainer');
-    comidas.forEach((element) => {
+
+    [...foods].forEach((element) => {
         foodsContainer.innerHTML += `
         <div class="col-md-4" key=${element.id}>
           <div class="card bg-dark border border-2 border-light border-opacity-25 mx-auto"
@@ -40,6 +18,10 @@ const comidas = [
         </div>
       `;
     });
-  };
+  }
+  
+  /*const foods = () => {
+    
+  };*/
   
   export default foods;
