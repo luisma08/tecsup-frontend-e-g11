f883ce2d6a60768d0ec616b7fbd816012f31875e
Jorge Mayta <luismayta08@gmail.com>
2022-12-21 22:17:09 -0500

    update foods js

--- semana-8/3-chanchirata/js/modules/foods.js
+++ semana-8/3-chanchirata/js/modules/foods.js
@@ -3,16 +3,17 @@
     const foodsContainer = document.getElementById('foodsContainer');
 
     [...foods].forEach((element) => {
+        const { id, name, description, urlImage, urlRecipe } = element;
         foodsContainer.innerHTML += `
-        <div class="col-md-4" key=${element.id}>
+        <div class="col-md-4" key=${id}>
           <div class="card bg-dark border border-2 border-light border-opacity-25 mx-auto"
             style="width: min(100%, 18rem);">
-            <img src="${element.urlImage}" class="card-img-top" alt="Anime 1" width="256"
+            <img src="${urlImage}" class="card-img-top" alt="Anime 1" width="256"
               height="144" style="width: 100%; height: 192px; object-fit: cover;">
             <div class="card-body">
-              <h5 class="card-title">${element.name}</h5>
-              <p class="card-text">${element.description}</p>
-              <a href="${element.urlRecipe}" class="btn btn-primary">Receta</a>
+              <h5 class="card-title">${name}</h5>
+              <p class="card-text">${description}</p>
+              <a href="${urlRecipe}" class="btn btn-primary">Receta</a>
             </div>
           </div>
         </div>
