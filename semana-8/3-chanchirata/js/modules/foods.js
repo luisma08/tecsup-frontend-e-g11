51f6ee120e94c51f911aff33213bd3a642c7d20d
Jorge Mayta <luismayta08@gmail.com>
2022-12-21 21:16:51 -0500

    update foots.js

--- semana-8/3-chanchirata/js/modules/foods.js
+++ semana-8/3-chanchirata/js/modules/foods.js
@@ -1,6 +1,45 @@
-
-const foods = () => {
-    console.log('Comida');
-};
-
-export default foods;
+const comidas = [
+    {
+      "id": 1,
+      "name": "Ají de Gallina",
+      "description": "El ají de gallina es un plato típico de la gastronomía peruana, concretamente de la costa, ​ que consiste en un ají o crema espesa con pechuga de gallina deshilachada. Esta crema es servida con papas cocidas, y en ocasiones con arroz blanco. Es común reemplazar la gallina por pollo.",
+      "urlImage": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Ají_de_gallina.jpg/800px-Ají_de_gallina.jpg",
+      "urlRecipe": "https://www.recetasnestle.com.pe/recetas/aji-de-gallina-2"
+    },
+    {
+      "id": 2,
+      "name": "Lomo Saltado",
+      "description": "El ají de gallina es un plato típico de la gastronomía peruana, concretamente de la costa, ​ que consiste en un ají o crema espesa con pechuga de gallina deshilachada. Esta crema es servida con papas cocidas, y en ocasiones con arroz blanco. Es común reemplazar la gallina por pollo.",
+      "urlImage": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Ají_de_gallina.jpg/800px-Ají_de_gallina.jpg",
+      "urlRecipe": "https://www.recetasnestle.com.pe/recetas/aji-de-gallina-2"
+    },
+    {
+      "id": 3,
+      "name": "Chaufa",
+      "description": "El ají de gallina es un plato típico de la gastronomía peruana, concretamente de la costa, ​ que consiste en un ají o crema espesa con pechuga de gallina deshilachada. Esta crema es servida con papas cocidas, y en ocasiones con arroz blanco. Es común reemplazar la gallina por pollo.",
+      "urlImage": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Ají_de_gallina.jpg/800px-Ají_de_gallina.jpg",
+      "urlRecipe": "https://www.recetasnestle.com.pe/recetas/aji-de-gallina-2"
+    }
+  ];
+  
+  const foods = () => {
+    const foodsContainer = document.getElementById('foodsContainer');
+    comidas.forEach((element) => {
+        foodsContainer.innerHTML += `
+        <div class="col-md-4" key=${element.id}>
+          <div class="card bg-dark border border-2 border-light border-opacity-25 mx-auto"
+            style="width: min(100%, 18rem);">
+            <img src="${element.urlImage}" class="card-img-top" alt="Anime 1" width="256"
+              height="144" style="width: 100%; height: 192px; object-fit: cover;">
+            <div class="card-body">
+              <h5 class="card-title">${element.name}</h5>
+              <p class="card-text">${element.description}</p>
+              <a href="${element.urlRecipe}" class="btn btn-primary">Receta</a>
+            </div>
+          </div>
+        </div>
+      `;
+    });
+  };
+  
+  export default foods;
