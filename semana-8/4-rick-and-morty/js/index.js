f17df2b4cd25a6f4945ecf7e703a8d85528a094e
Jorge Mayta <luismayta08@gmail.com>
2022-12-22 21:26:27 -0500

    add axios

--- semana-8/4-rick-and-morty/js/index.js
+++ semana-8/4-rick-and-morty/js/index.js
@@ -4,12 +4,38 @@ import characters from "./modules/characters.js";
 
 const documetReady = () => {
     //Obtener dato de forma asincrona
+    
     const fetchApi = async () => {
         //await esperar para obtener el dato
-        const response = await fetch('http://127.0.0.1:5501/semana-8/3-chanchirata/json/foods.json');//End point
+        /*
+        const response = await fetch('https://rickandmortyapi.com/api/character/?page=1');//End point
         const data = await response.json();// Espera y parsea a json
         characters(data);
+        */
+       /* Axios
+        axios.get('https://rickandmortyapi.com/api/character/?page=1')
+        .then(function (response) {
+            // handle success
+            console.log(response);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .finally(function () {
+            // always executed
+        });
+        */
+       try {
+            const { data } = await axios.get('https://rickandmortyapi.com/api/character/?page=1');
+            characters(data);
+       } catch (error) {
+            console.log(error);
+       } finally {
+        console.log('Se obtuvo el recurso');
+       }
     };
+    
     fetchApi();
 };
 document.addEventListener('DOMContentLoaded', documetReady);
--- semana-8/4-rick-and-morty/js/modules/characters.js
+++ semana-8/4-rick-and-morty/js/modules/characters.js
@@ -1,19 +1,19 @@
 
-  const characters = ({ foods }) => {
+  const characters = ({ results }) => {
     const foodsContainer = document.getElementById('characterContainer');
 
-    [...foods].forEach((element) => {
-        const { id, name, description, urlImage, urlRecipe } = element;
+    [...results].forEach((element) => {
+        const { id, name, location, image, url } = element;
         foodsContainer.innerHTML += `
         <div class="col-md-4" key=${id}>
           <div class="card bg-dark border border-2 border-light border-opacity-25 mx-auto"
             style="width: min(100%, 18rem);">
-            <img src="${urlImage}" class="card-img-top" alt="Anime 1" width="256"
+            <img src="${image}" class="card-img-top" alt="Anime 1" width="256"
               height="144" style="width: 100%; height: 192px; object-fit: cover;">
             <div class="card-body">
               <h5 class="card-title">${name}</h5>
-              <p class="card-text">${description}</p>
-              <a href="${urlRecipe}" class="btn btn-primary">Receta</a>
+              <p class="card-text">${location.name}</p>
+              <a href="${url}" class="btn btn-primary">Api</a>
             </div>
           </div>
         </div>
