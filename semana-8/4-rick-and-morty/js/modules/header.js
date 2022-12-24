5299901c9ebab26235c76a3946913c7eeabeb0b9
Jorge Mayta <luismayta08@gmail.com>
2022-12-23 22:47:14 -0500

    move function renderCharacters in utils and export

--- semana-8/4-rick-and-morty/js/modules/header.js
+++ semana-8/4-rick-and-morty/js/modules/header.js
@@ -1,3 +1,5 @@
+import renderCharacters from "../utils/utils.js";
+
 const header = () => {
     const searchForm = document.getElementById('searchForm');
 
@@ -7,46 +9,11 @@ const header = () => {
         fetchApi(name);
     };
 
-    const characters = ({ results }) => {
-        const charactersContainer = document.getElementById('characterContainer');
-        charactersContainer.innerHTML = '';
-    
-        [...results].forEach((element) => {
-            const { id, name, status, species, type, gender, origin, location, image } = element;
-            charactersContainer.innerHTML += `
-            <div class="col-md-4" key=${id}>
-              <div class="card bg-dark border border-2 border-light border-opacity-25 h-100 mx-auto"
-                style="width: min(100%, 18rem);">
-                <div class="card-header text-center">
-                  ${name}
-                </div>
-                <div class="card-body">
-                  <img src="${image}" class="card-img-top" alt="Anime 1" width="256" height="144"
-                    style="width: 100%; height: 192px; object-fit: cover; border-radius: 0.5rem"/>
-                  <h5 class="card-title mt-2">${name}</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">${type || 'Chanchito'}</h6>
-                  <p class="card-text">
-                    <span class="badge text-bg-info">${status}</span>
-                    <span class="badge text-bg-primary">${species}</span>
-                    <span class="badge text-bg-secondary">${gender}</span>
-                  </p>
-                  <p class="card-text"><span class="text-info fw-bold">Origen:</span> ${origin.name}</p>
-                  <p class="card-text"><span class="text-info fw-bold">Ubicación:</span> ${location.name}</p>
-                </div>
-                <div class="card-footer text-center">
-                  <a href="${image}" target="_blank" rel="noopener noreferrer" class="card-link">Descargar imagen</a>
-                </div>
-              </div>
-            </div>
-          `;
-        });
-      };
-
     const fetchApi = async (name) => {
 
        try {
             const { data } = await axios.get(`https://rickandmortyapi.com/api/character/?name=${name}`);
-            characters(data);
+            renderCharacters(data);
        } catch (error) {
             console.log(error);
        } finally {
