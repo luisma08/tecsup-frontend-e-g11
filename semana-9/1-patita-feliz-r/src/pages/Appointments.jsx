15080676d4a822a9a4bd580a4112832b39edfdcf
Jorge Mayta <luismayta08@gmail.com>
2023-01-05 22:21:03 -0500

    useState, little hook

--- semana-9/1-patita-feliz-r/src/pages/Appointments.jsx
+++ semana-9/1-patita-feliz-r/src/pages/Appointments.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
+
 const Appointments = () => {
+
+    const [cuantity, setCuantity] = useState(1);
+
+    const createAppointment = () => {
+        setCuantity(cuantity + 1);
+    };
+
     return (
-        <h2>Appointments</h2>
+        <section className="py-4">
+            <div className="container d-flex justify-content-between">
+                <div className="badge text-bg-primary d-flex align-items-center gap-1 pe-auto">
+                Appointments <span className="badge text-bg-secondary">{cuantity}</span>
+                </div>
+                <button className="btn btn-primary" onClick={createAppointment}>Registrar cita</button>
+            </div>
+        </section>
     );
 }
 
