4c04df4f395c9d486f2aa40ba4e1169e636eae57
Jorge Mayta <luismayta08@gmail.com>
2023-01-05 20:57:06 -0500

    add props o properties

--- semana-9/1-patita-feliz-r/src/App.js
+++ semana-9/1-patita-feliz-r/src/App.js
@@ -2,8 +2,12 @@ import './App.css';
 import Header from './components/sections/header';
 
 function App() {
+  const company = {
+    name: 'Patito Feliz 🦆',
+    slogan: 'La mejor veterinaria del Peru'
+  };
   return (
-    <Header />
+    <Header company={company}/>
   );
 }
 
--- semana-9/1-patita-feliz-r/src/components/sections/header.jsx
+++ semana-9/1-patita-feliz-r/src/components/sections/header.jsx
@@ -1,9 +1,10 @@
-const Header = () => {
+const Header = ({ company }) => {
+    const { name, slogan } = company;
     return (
         <header className="py-3">
             <div className="container d-flex flex-column gap-1">
-                <h2 className="text-info text-center fs-2 m-0">Patito Feliz 🦆</h2>
-                <h1 className="text-primary text-center fs-5 m-0">La mejor veterinaria del Peru</h1>
+                <h2 className="text-info text-center fs-2 m-0">{name}</h2>
+                <h1 className="text-primary text-center fs-5 m-0">{slogan}</h1>
             </div>
         </header>
     );
