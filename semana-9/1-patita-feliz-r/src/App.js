f8931990c187ef5b107923d4998b202c0304ebf1
Jorge Mayta <luismayta08@gmail.com>
2023-01-05 21:13:21 -0500

    add footer section

--- semana-9/1-patita-feliz-r/src/App.js
+++ semana-9/1-patita-feliz-r/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import Header from './components/sections/header';
+import Footer from './components/sections/footer';
 
 function App() {
   const company = {
     name: 'Patito Feliz 🦆',
     slogan: 'La mejor veterinaria del Peru'
   };
+
+  const credits = {
+    year: new Date().getFullYear(),
+    author: 'Jorge Mayta'
+  };
+
   return (
-    <Header company={company}/>
+    <>
+      <Header company={company}/>
+      <Footer credits={credits}/>
+    </>
   );
 }
 
