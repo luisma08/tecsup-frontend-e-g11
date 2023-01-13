116a4dc1435bafdbb97df5597810872263e105a1
Jorge Mayta <luismayta08@gmail.com>
2023-01-12 19:22:34 -0500

    update header

--- semana-10/2-eakon-frontend/src/components/sections/Header.jsx
+++ semana-10/2-eakon-frontend/src/components/sections/Header.jsx
@@ -9,6 +9,22 @@ const Header = () => {
                         <img src={logoEakon} alt="Logo de EAKON" className="nav__logo" width="32" height="32" />
                     </a>
                 </div>
+                <div className="modal modal--header">
+                    <ul className="list list--header">
+                        <li>
+                            <a className="list__link">Productos</a>
+                        </li>
+                        <li>
+                            <a className="list__link">Servicios</a>
+                        </li>
+                        <li>
+                            <a className="list__link">Nosotros</a>
+                        </li>
+                        <li>
+                            <a className="list__link">Contacto</a>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         </header>
     );
