78c4681ca74eacf0e14e8afaf15c131501969e16
Jorge Mayta <luismayta08@gmail.com>
2023-01-13 21:38:07 -0500

    create functions in Header.jsx

--- semana-10/2-eakon-frontend/src/components/sections/Header.jsx
+++ semana-10/2-eakon-frontend/src/components/sections/Header.jsx
@@ -1,15 +1,34 @@
+import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import logoEAKON from '../../assets/img/logo-eakon.webp';
 
 const Header = () => {
+    const header = useRef();
+    const modal = useRef();
+
+    const showModal = () => {
+      header.current.classList.add('header--menu');
+      modal.current.classList.add('modal--show');
+    }
+
+    const removeModal = () => {
+      header.current.classList.remove('header--menu');
+      modal.current.classList.remove('modal--show');
+    }
+
     return (
-        <header className="header">
+        <header className="header" 
+        ref={header}
+        >
           <nav className="nav">
             <div className="container f-elements f-elements--header">
               <Link to="/">
                 <img src={logoEAKON} alt="Logo de EAKON" className="nav__logo" width="32" height="32" />
               </Link>
-              <div className="modal modal--header">
+              <div className="modal modal--header" 
+              ref={modal}
+              onClick={removeModal}
+              >
                 <ul className="list list--header">
                   <li>
                     <Link to="/productos" className="list__link">Productos</Link>
@@ -31,7 +50,7 @@ const Header = () => {
                     <option value="value2">☀</option>
                     <option value="value3">💻</option>
                 </select>
-                <button className='icon'>
+                <button className='icon' onClick={showModal}>
                     🍔
                 </button>
               </div>
