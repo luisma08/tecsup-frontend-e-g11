1eafbfb63cc31890d6c343a3a1b73488e30880ea
Jorge Mayta <luismayta08@gmail.com>
2023-01-18 19:38:57 -0500

    update styles

--- semana-10/2-eakon-frontend/src/components/sections/Header.jsx
+++ semana-10/2-eakon-frontend/src/components/sections/Header.jsx
@@ -1,77 +1,103 @@
 import { useRef } from 'react';
-import { Link, NavLink } from 'react-router-dom';
 import { BsList, BsXCircle } from "react-icons/bs";
+import { Link, NavLink } from 'react-router-dom';
 import logoEAKON from '../../assets/img/logo-eakon.webp';
 
 const Header = () => {
-    const header = useRef();
-    const modal = useRef();
 
-    const showModal = () => {
-      header.current.classList.add('header--move');
-      modal.current.classList.add('modal--show');
-    }
+  const header = useRef();
+  const modal = useRef();
 
-    const removeModal = () => {
-      header.current.classList.remove('header--move');
-      modal.current.classList.remove('modal--show');
-    }
+  const showMenu = () => {
+    header.current.classList.add('right-50');
+    modal.current.classList.add('modal--show');
+  };
 
-    return (
-        <header className="header" 
-        ref={header}
-        >
-          <nav className="nav">
-            <div className="container f-elements f-elements--header">
-              <Link to="/">
-                <img src={logoEAKON} alt="Logo de EAKON" className="nav__logo" width="32" height="32" />
-              </Link>
-              <div className="modal modal--header" 
-              ref={modal}
-              onClick={removeModal}
-              >
-                <ul className="list list--header" 
-                onClick={e => e.stopPropagation()}
+  const hideMenu = () => {
+    header.current.classList.remove('right-50');
+    modal.current.classList.remove('modal--show');
+  };
+
+  return (
+    <header
+      className="header"
+      ref={header}
+    >
+      <nav className="nav">
+        <div className="container f-elements f-elements--header">
+          <Link to="/">
+            <img src={logoEAKON} alt="Logo de EAKON" className="nav__logo" width="32" height="32" />
+          </Link>
+          <div
+            className="modal modal--header"
+            ref={modal}
+            onClick={hideMenu}
+          >
+            <ul
+              className="list list--header f-elements f-elements--responsive f-elements--center"
+              onClick={e => e.stopPropagation()}
+            >
+              <li>
+                <NavLink
+                  to="/productos"
+                  className={({ isActive }) => isActive ? "list__link list__link--active" : "list__link"}
+                  onClick={hideMenu}
                 >
-                  <li>
-                    <NavLink to="/productos" 
-                    className={({ isActive }) => isActive ? "list__link list__link--active" : "list__link"}
-                    onClick={removeModal}>Productos</NavLink>
-                  </li>
-                  <li>
-                    <NavLink to="/servicios" 
-                    className={({ isActive }) => isActive ? "list__link list__link--active" : "list__link"}
-                    onClick={removeModal}>Servicios</NavLink>
-                  </li>
-                  <li>
-                    <NavLink to="/nosotros" 
-                    className={({ isActive }) => isActive ? "list__link list__link--active" : "list__link"}
-                    onClick={removeModal}>Nosotros</NavLink>
-                  </li>
-                  <li>
-                    <NavLink to="/contacto" 
-                    className={({ isActive }) => isActive ? "list__link list__link--active" : "list__link"}
-                    onClick={removeModal}>Contacto</NavLink>
-                  </li>
-                  <li className='icon icon--close' onClick={removeModal}>
-                    <BsXCircle />
-                  </li>
-                </ul>
-              </div>
-              <div className="f-elements f-elements--center">
-                <select name="" id="">
-                    <option value="value1">🌑</option>
-                    <option value="value2">☀</option>
-                    <option value="value3">💻</option>
-                </select>
-                <button className='icon' onClick={showModal}>
-                    <BsList />
-                </button>
-              </div>
-            </div>
-          </nav>
-        </header>
-    );
+                  Productos
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/servicios"
+                  className={({ isActive }) => isActive ? "list__link list__link--active" : "list__link"}
+                  onClick={hideMenu}
+                >
+                  Servicios
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/nosotros"
+                  className={({ isActive }) => isActive ? "list__link list__link--active" : "list__link"}
+                  onClick={hideMenu}
+                >
+                  Nosotros
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/contacto"
+                  className={({ isActive }) => isActive ? "list__link list__link--active" : "list__link"}
+                  onClick={hideMenu}
+                >
+                  Contacto
+                </NavLink>
+              </li>
+              <li
+                className="icon icon--close"
+                onClick={hideMenu}
+              >
+                <BsXCircle />
+              </li>
+            </ul>
+          </div>
+          <div className="f-elements f-elements--center">
+            {/* <select>
+              <option value="value1">🌚</option>
+              <option value="value2">🌞</option>
+              <option value="value3">💻</option>
+            </select> */}
+            <button
+              className="icon icon--menu"
+              onClick={showMenu}
+            >
+              <BsList />
+            </button>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
