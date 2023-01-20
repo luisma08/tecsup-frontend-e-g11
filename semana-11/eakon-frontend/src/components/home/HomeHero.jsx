3c1ef935dfde5fcb88eaf6734bf1d615d2b632a9
Jorge Mayta <luismayta08@gmail.com>
2023-01-19 23:27:36 -0500

    add styles in line

--- semana-11/eakon-frontend/src/components/home/HomeHero.jsx
+++ semana-11/eakon-frontend/src/components/home/HomeHero.jsx
@@ -1,22 +1,42 @@
-// Import Swiper React components
+// import Swiper core and required modules
+import { Navigation, Pagination, A11y, Autoplay } from 'swiper';
+
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const HomeHero = ({ heroProducts }) => {
     return (
-            <Swiper
-        spaceBetween={50}
-        slidesPerView={3}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)}
-        >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        </Swiper>
+        <Swiper
+        tag='section'
+        wrapperTag='div'
+        className='hero'
+        modules={[Navigation, Autoplay, Pagination, A11y]}
+        autoplay={{delay: 5000}}
+        navigation
+        pagination={{ clickable: true }}
+        preloadImages={false}
+      >
+        {heroProducts.map((element)  => {
+            const { nombre, imagen } = element.attributes;
+            return(<SwiperSlide 
+                key={element.id}
+                tag='section'
+                style={{
+                    backgroundImage: `url(${imagen.data[0].attributes.url})`,
+                    backgroundRepeat:"no-repeat",
+                    objectFit: 'cover',
+                    height: '100vh'
+                }}
+            >
+                {nombre}
+            </SwiperSlide>)
+        })}
+      </Swiper>
     );
 };
 
